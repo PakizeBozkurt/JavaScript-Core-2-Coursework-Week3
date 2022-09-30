f7bef82762023203491bb55da24818b43cea3619
Pakize Bozkurt <pakiozihak@gmail.com>
2022-09-30 10:03:37 +0100

    Completed 2-Project

--- 1-exercises/B-todo-list/script.js
+++ 1-exercises/B-todo-list/script.js
@@ -1,20 +1,20 @@
 function todoList(todos) {
-  let content = document.querySelector("#content");
-  let unOrderedList = document.createElement("ul");
-  content.appendChild(unOrderedList);
+   let content = document.querySelector("#content");
+   let unOrderedList = document.createElement("ul");
+   content.appendChild(unOrderedList);
   todos.forEach((item) => {
-    let list = document.createElement("li");
-    list.innerHTML = item.todo;
-    unOrderedList.appendChild(list);
-    list.addEventListener("click", () => {
-      if (list.style.textDecoration === "line-through") {
-        list.style.textDecoration = "";
-      } else {
-        list.style.textDecoration = "line-through";
-      }
-    });
-  });
-}
+     let list = document.createElement("li");
+     list.innerHTML = item.todo;
+     unOrderedList.appendChild(list);
+     list.addEventListener("click", () => {
+       if (list.style.textDecoration === "line-through") {
+         list.style.textDecoration = "";
+       } else {
+         list.style.textDecoration = "line-through";
+       }
+           });
+   });
+ }
 
 const todos = [
   { todo: "wash the dishes" },
--- 2-projects/1-todo-list/script.js
+++ 2-projects/1-todo-list/script.js
@@ -1,6 +1,19 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  for (let todo of todos) {
+    const item = ` <li
+                class="list-group-item d-flex justify-content-between align-items-center"
+              >
+                ${todo.task}
+                <span class="badge bg-primary rounded-pill">
+                  <!-- each of these <i> tags will need an event listener when we create them in Javascript -->
+                  <i class="fa fa-check" aria-hidden="true"></i>
+                  <i class="fa fa-trash" aria-hidden="true"></i>
+                </span>
+              </li>`;
+    list.innerHTML += item;
+  }
 }
 
 // These are the same todos that currently display in the HTML
@@ -14,12 +27,43 @@ populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
+  let list = document.getElementById("todo-list");
+  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  let input = document.querySelector("#todoInput");
+  const item = `<li
+                class="list-group-item d-flex justify-content-between align-items-center"
+              >
+                ${input.value}
+                <span class="badge bg-primary rounded-pill">
+                  <!-- each of these <i> tags will need an event listener when we create them in Javascript -->
+                  <i class="fa fa-check" aria-hidden="true"></i>
+                  <i class="fa fa-trash" aria-hidden="true"></i>
+                </span>
+              </li>`;
+  list.innerHTML += item;
+  input.value = "";
+
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let input = document.querySelector(".form-control");
+  let inputValue = [
+    {
+      task: input.value,
+      completed: false,
+    },
+  ];
+  populateTodoList(inputValue);
+  input.value = "";
 }
-
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
+  let toDoTask = document.querySelectorAll("li");
+  toDoTask.forEach((task) => {
+    if (task.classList.contains("line-through")) {
+      task.remove();
+    }
+  });
 }
+setInterval(deleteAllCompletedTodos, 1000);
